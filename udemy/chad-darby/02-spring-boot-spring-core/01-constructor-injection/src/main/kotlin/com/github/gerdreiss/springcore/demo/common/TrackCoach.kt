d0d3c25f86978e4d7cb75caa2516455ca83cf90e
Gerd Reiss <gerd@reiss.pro>
2026-05-25 08:32:04 +0200

    Spring Boot 4, Spring 7 & Hibernate - bean lifecycle

--- udemy/chad-darby/02-spring-boot-spring-core/01-constructor-injection/src/main/kotlin/com/github/gerdreiss/springcore/demo/common/TrackCoach.kt
+++ udemy/chad-darby/02-spring-boot-spring-core/01-constructor-injection/src/main/kotlin/com/github/gerdreiss/springcore/demo/common/TrackCoach.kt
@@ -1,6 +1,10 @@
 package com.github.gerdreiss.springcore.demo.common
 
 import com.github.gerdreiss.springcore.util.DateProvider
+import jakarta.annotation.PostConstruct
+import jakarta.annotation.PreDestroy
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Primary
 import org.springframework.stereotype.Component
 import java.time.format.DateTimeFormatter
@@ -11,9 +15,17 @@ class TrackCoach : Coach {
 
     private val dateProvider: DateProvider
 
+    private val logger: Logger? = LoggerFactory.getLogger(javaClass)
+
     constructor(dateProvider: DateProvider) {
         this.dateProvider = dateProvider
     }
+
+    @PostConstruct
+    fun init() = logger?.info("track coach initialized")
+
+    @PreDestroy
+    fun destroy() = logger?.info("destroying track coach")
 
     override fun getDailyWorkout(): String =
         "[${DateTimeFormatter.ISO_DATE.format(this.dateProvider.getCurrentDate())}]: Run a hard 5k"
