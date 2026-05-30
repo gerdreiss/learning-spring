84ab4b014918f7ee3aedbd6d02048a8605c79b6b
Gerd Reiss <gerd@reiss.pro>
2026-05-30 16:23:59 +0200

    Spring Boot 4, Spring 7 & Hibernate - REST CRUD APIs using SPRING DATA REST

--- udemy/chad-darby/04-spring-boot-rest-crud/03-spring-boot-rest-data-crud-employee/src/main/kotlin/com/github/gerdreiss/cruddemo/repository/EmployeeRepository.kt
+++ udemy/chad-darby/04-spring-boot-rest-crud/03-spring-boot-rest-data-crud-employee/src/main/kotlin/com/github/gerdreiss/cruddemo/repository/EmployeeRepository.kt
@@ -2,7 +2,7 @@ package com.github.gerdreiss.cruddemo.repository
 
 import com.github.gerdreiss.cruddemo.entity.Employee
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
+import org.springframework.data.rest.core.annotation.RepositoryRestResource
 
-@Repository
+@RepositoryRestResource(path = "employees")
 interface EmployeeRepository : JpaRepository<Employee, Int>
